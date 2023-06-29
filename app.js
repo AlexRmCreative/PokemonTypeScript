c9953b1fde812bf1dbab55c5446e63752339a33b
AlexRmCreative <alejandro.roa.mateo@gmail.com>
2023-06-29 23:42:42 +0200

    Start

--- app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var readline_sync_1 = require("readline-sync");
-console.log('Hello world');
-readline_sync_1.default.question('Presiona Enter para salir...');
