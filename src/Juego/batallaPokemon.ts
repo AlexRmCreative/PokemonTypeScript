c3460aa41621e8582ca80b8ff4e1480361bcdaa9
AlexRmCreative <alejandro.roa.mateo@gmail.com>
2023-07-03 13:17:48 +0200

    Seleccionar Pokemon

--- src/Juego/batallaPokemon.ts
+++ src/Juego/batallaPokemon.ts
@@ -3,21 +3,29 @@ import { Jugador } from '../Clases/Jugador';
 
 export function BatallaPokemon(pokemon: Pokemon[]) {
     let turno = 0;
-    console.log(pokemon[1].Nombre);
-    pokemon[1].Aranazo(pokemon[0]);
+    //Primer turno para el pokemon mas veloz
     if (pokemon[0].Velocidad < pokemon[1].Velocidad)
         turno = 1;
 
-    /*//Mientras hayan pokemons la batalla continuara
+    //Mientras hayan pokemons la batalla continuara
     do {
-        //Comprueba si la vida de algun pokemon ha bajado a 0
+        //Comprueba si la vida de algun pokemon ha bajado a 0 para asi eliminarlo de la lista
         for (let i = 0; i < pokemon.length;) {
             pokemon[i].Vida < 0 ? pokemon.splice(i, 1) : i++;
         }
+
+        //SeleccionarHabilidad();
+
         //Cambiar de turno
         turno == pokemon.length ? turno = 0 : turno++;
     }
-    while (pokemon.length > 1)*/
-    console.log(`${pokemon[0].Nombre} gana!`);
-    pokemon[0].HacerRuido();
+    while (pokemon.length > 1)
+
+    /*console.log(`${pokemon[0].Nombre} gana!`);
+    pokemon[0].HacerRuido();*/
+}
+
+function SeleccionarHabilidad()
+{
+
 }
--- src/Juego/juego.ts
+++ src/Juego/juego.ts
@@ -2,23 +2,26 @@
 import { Jugador } from '../Clases/Jugador';
 import { BatallaPokemon } from './batallaPokemon';
 
+
 export function Juego() {
     const charmander: Pokemon = new Pokemon("Charmander", "Fuego", "Char-chaar!");
     const squirtle: Pokemon = new Pokemon("Squirtle", "Agua", "Squirtle, Squirtle!");
     const bulbasaur: Pokemon = new Pokemon("Bulbasaur", "Planta", "bulbasaur...");
     let pokemonsDisponibles: Pokemon[] = [charmander, squirtle, bulbasaur];
     const jugador: Jugador = new Jugador();
-    let selector: number = 0;
 
     console.log(`Hola ${jugador.Nombre}, Bienvenido a esta batalla Pokémon desarollada con TypeScript`);
-    console.log(`Elige un pokémon`);
+    SeleccionarPokemon(jugador, pokemonsDisponibles);
+    BatallaPokemon(pokemonsDisponibles);
+}
 
+function SeleccionarPokemon(jugador: Jugador, pokemonsDisponibles: Pokemon[]) {
+    let selector: number = 0;
+
+    console.log(`Selecciona un pokémon`);
     pokemonsDisponibles.forEach((pokemon: Pokemon, index: number) => {
         if (index != selector) console.log(`♦ ${pokemon.Nombre}`);
         else
             console.log(`♦ ${pokemon.Nombre} ←`);
     });
-
-    console.log();
-    BatallaPokemon(pokemonsDisponibles);
 }
